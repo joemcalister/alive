86fed041e52273eda01cd86a4b8a819ad94d3b35
Joe McAlister <joe@lonestorm.com>
2017-02-23 15:37:12 +0000

    Updated file structure

---
+++ sign/sign.sh
@@ -0,0 +1,42 @@
+# compile the 
+cd ../generate
+cmake .
+make
+./verifybot
+
+mv frame.png ../sign/verification.png
+
+cd ../sign
+gpg --sign verification.png
+
+
+exit 1;
+
+mkdir signing
+cd signing
+echo "$1" >> temp_sig.txt
+
+# gpg sign message
+gpg --output to_upload.sig --clearsign temp_sig.txt
+
+# git clone old repo
+git clone https://github.com/joemcalister/alive.git &> /dev/null
+
+# move file into it
+mv to_upload.sig alive/signed.sig
+cd alive
+
+# commit the update
+git commit -S -am "Updated verification" &> /dev/null
+
+# push it
+git push -u origin master &> /dev/null
+
+# change back
+cd ../../
+
+# delete everything
+sudo rm -r signing
+
+# echo done
+echo "VERIFIED"
